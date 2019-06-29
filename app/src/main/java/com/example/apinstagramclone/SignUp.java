5c321ad4111f277390ce5220aeae46327a2abecb
kakadoo91 <kakadoo91@gmail.com>
2019-06-30 01:15:55 +0200

    error dare

--- app/src/main/java/com/example/apinstagramclone/SignUp.java
+++ app/src/main/java/com/example/apinstagramclone/SignUp.java
@@ -5,17 +5,27 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.parse.FindCallback;
+import com.parse.GetCallback;
+import com.parse.GetDataCallback;
 import com.parse.ParseException;
 import com.parse.ParseObject;
+import com.parse.ParseQuery;
 import com.parse.SaveCallback;
 import com.shashank.sony.fancytoastlib.FancyToast;
+
+import java.util.List;
 
 public class SignUp extends AppCompatActivity implements View.OnClickListener {
 
      private Button btnSave;
-     private EditText edtName,edtPunchSpeed,edtPunchPwer,edtKickSpeed,edtKickPower;
+     private EditText edtName,edtPunchSpeed,edtPunchPower,edtKickSpeed,edtKickPower;
+     private TextView txtGetData;
+     private Button btnGetAllDatat;
+     private String allKickBoxers;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,20 +36,68 @@ public class SignUp extends AppCompatActivity implements View.OnClickListener {
 
         edtName = findViewById(R.id.edtName);
         edtPunchSpeed = findViewById(R.id.edtPunchspeed);
-        edtPunchPwer = findViewById(R.id.edtPunchPower);
+        edtPunchPower = findViewById(R.id.edtPunchPower);
         edtKickSpeed = findViewById(R.id.edtKickSpeed);
         edtKickPower = findViewById(R.id.edtKickPower);
+        txtGetData = findViewById(R.id.txtGetData);
+        btnGetAllDatat = findViewById(R.id.btnGetAllData);
+        txtGetData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+                ParseQuery<ParseObject> parseQuery = ParseQuery.getQuery("newKickBoxer");
+                parseQuery.getInBackground("uvkEve4PlA", new GetCallback<ParseObject>() {
+                    @Override
+                    public void done(ParseObject object, ParseException e) {
+
+                        if (object != null && e == null){
+
+                            txtGetData.setText(object.get("name") + " " );
+                        }
+
+                    }
+                });
+            }
+        });
+    btnGetAllDatat.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            allKickBoxers = "";
+
+            ParseQuery<ParseObject> queryAll = ParseQuery.getQuery("newKickBoxer");
+            queryAll.findInBackground(new FindCallback<ParseObject>() {
+                @Override
+                public void done(List<ParseObject> objects, ParseException e) {
+                    if(e == null){
+                        if(objects.size() > 0 ){
+
+                            for (ParseObject newkickBoxer :objects){
+                                allKickBoxers = allKickBoxers + newkickBoxer.get("name") + "\n";
+                            }
+
+                            FancyToast.makeText(SignUp.this,allKickBoxers, FancyToast.LENGTH_LONG, FancyToast.SUCCESS, true).show();
+                        }
+                       else{
+                            FancyToast.makeText(SignUp.this, e.getMessage(), FancyToast.LENGTH_LONG, FancyToast.ERROR, true).show();
+
+                       }
+                    }
+
+                }
+            });
+
+        }
+    });
     }
 
     @Override
     public void onClick(View v) {
         try {
 
-            final ParseObject kickBoxer = new ParseObject("kickBoxer");
+            final ParseObject kickBoxer = new ParseObject("newkickBoxer");
             kickBoxer.put("name", edtName.getText().toString());
-            kickBoxer.put("punchSpeed", Integer.parseInt(edtKickSpeed.getText().toString()));
-            kickBoxer.put("punchPower", Integer.parseInt(edtKickPower.getText().toString()));
+            kickBoxer.put("punchSpeed", Integer.parseInt(edtPunchSpeed.getText().toString()));
+            kickBoxer.put("punchPower", Integer.parseInt(edtPunchPower.getText().toString()));
             kickBoxer.put("kickSpeed", Integer.parseInt(edtKickSpeed.getText().toString()));
             kickBoxer.put("kickPower", Integer.parseInt(edtKickPower.getText().toString()));
             kickBoxer.saveInBackground(new SaveCallback() {
